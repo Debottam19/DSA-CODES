14f13e769c30bdc076c552edc9fb7db1edd3fde8
Debottam19 <125583945+Debottam19@users.noreply.github.com>
2023-07-03 21:44:24 +0530

    Add files via upload

---
+++ MULTIPLE_PARENTHESIS_MATCHING.cpp
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct stack
+{
+    int size;
+    int top;
+    char *arr;
+};
+int IsFull(struct stack *ptr)
+{
+    if(ptr->top==ptr->size-1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int IsEmpty(struct stack *ptr)
+{
+    if(ptr->top==-1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+void Push(struct stack *ptr,char symbol)
+{
+    if(IsFull(ptr))
+    {
+        printf("STACK IS OVERFLOWN!!!!!!");
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = symbol;
+    }
+}
+char Pop(struct stack *ptr)
+{
+    char PopedSymbol; 
+    if(IsEmpty(ptr))
+    {
+        printf("STACK IS UNDERFLOWN!!!!!!");
+    }
+    else
+    {
+        PopedSymbol = ptr->arr[ptr->top];
+        ptr->top--;   
+    }
+    return PopedSymbol;
+}
+int MatchingBrackets(char a,char b)
+{
+    if(a == '(' && b == ')')
+    {
+        return 1;
+    }
+    else if(a == '{' && b == '}')
+    {
+        return 1;
+    }
+    else if(a == '[' && b == ']')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int ParenthdsisMathching(char *exp)
+{
+    int i = 0;
+    char PopedBracket;
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = strlen(exp);
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size*sizeof(char));
+    for ( i = 0; exp[i]!='\0'; i++)
+    {
+        if(exp[i]=='(' || exp[i]=='{' || exp[i]=='[')
+        {
+            Push(sp,exp[i]);
+        }
+        else if(exp[i]==')' || exp[i]=='}' || exp[i]==']')
+        {
+            if(IsEmpty(sp))
+            {
+                return 0;
+            }
+            else
+            {
+                PopedBracket = Pop(sp);
+                if(MatchingBrackets(PopedBracket,exp[i]))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+    if(IsEmpty(sp))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    char ch[100];
+    printf("Enter the expression : \n");
+    scanf("%s",ch);
+    if(ParenthdsisMathching(ch))
+    {
+        printf("Parenthesis is matching for the expression %s",ch);
+    }
+    else
+    {
+        printf("Parenthesis is not matching for the expression %s",ch);
+    }
+
+    return 0;
+
+}
